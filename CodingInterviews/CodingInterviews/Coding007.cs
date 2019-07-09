1cdc30e217173cd6d0dd484a2daee945b7e3b028
jason.hua <jason.hua@PC-JASONHUA>
2019-07-09 16:16:15 +0800

    斐波那契数列

---
+++ CodingInterviews/CodingInterviews/Coding007.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    /// <summary>
+    /// 递归和循环:斐波那契数列
+    /// 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。 n<=39
+    /// </summary>
+    public class Coding007
+    {
+        /// <summary>
+        /// 递归实现
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Fibonacci(int n) {
+            if (n <= 1) return n;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        /// <summary>
+        /// 循环实现
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Fibonacci2(int n)
+        {
+            if (n <= 1) return n;
+
+            int first = 0;
+            int second = 1;
+            int result = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 求和
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int FibonacciSum(int n) {
+            if (n <= 1) return n;
+            int first = 0;
+            int second = 1;
+            int temp = 0;
+            int result = first + second;
+            for (int i = 2; i <= n; i++) {
+                temp = first + second;
+                first = second;
+                second = temp;
+
+                result = result + temp;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 利用公式求和
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int FibonacciSum2(int n)
+        {
+            if (n <= 1) return n;
+            int first = 0;
+            int second = 1;
+            int temp = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                temp = first + second;
+                first = second;
+                second = temp;
+            }
+
+            int result = 2 * second + first - 1; //Sn = 2an + an - 1 - 1
+
+            return result;
+        }
+    }
+}
---
+++ CodingInterviews/CodingTest/Coding007Test.cs
@@ -0,0 +1,75 @@
+﻿using CodingInterviews;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingTest
+{
+    public class Coding007Test
+    {
+        [Fact]
+        public void Test0()
+        {
+            Assert.Equal(0, Coding007.Fibonacci(0));
+            Assert.Equal(0, Coding007.Fibonacci2(0));
+            Assert.Equal(0, Coding007.FibonacciSum(0));
+            Assert.Equal(0, Coding007.FibonacciSum2(0));
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, Coding007.Fibonacci(1));
+            Assert.Equal(1, Coding007.Fibonacci2(1));
+            Assert.Equal(1, Coding007.FibonacciSum(1));
+            Assert.Equal(1, Coding007.FibonacciSum2(1));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, Coding007.Fibonacci(2));
+            Assert.Equal(1, Coding007.Fibonacci2(2));
+            Assert.Equal(2, Coding007.FibonacciSum(2));
+            Assert.Equal(2, Coding007.FibonacciSum2(2));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal(2, Coding007.Fibonacci(3));
+            Assert.Equal(2, Coding007.Fibonacci2(3));
+            Assert.Equal(4, Coding007.FibonacciSum(3));
+            Assert.Equal(4, Coding007.FibonacciSum2(3));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.Equal(3, Coding007.Fibonacci(4));
+            Assert.Equal(3, Coding007.Fibonacci2(4));
+            Assert.Equal(7, Coding007.FibonacciSum(4));
+            Assert.Equal(7, Coding007.FibonacciSum2(4));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Assert.Equal(5, Coding007.Fibonacci(5));
+            Assert.Equal(5, Coding007.Fibonacci2(5));
+            Assert.Equal(12, Coding007.FibonacciSum(5));
+            Assert.Equal(12, Coding007.FibonacciSum2(5));
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            Assert.Equal(8, Coding007.Fibonacci(6));
+            Assert.Equal(8, Coding007.Fibonacci2(6));
+            Assert.Equal(20, Coding007.FibonacciSum(6));
+            Assert.Equal(20, Coding007.FibonacciSum2(6));
+        }
+
+    }
+}
