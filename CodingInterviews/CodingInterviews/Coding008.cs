97d444d1017218170ca5027c271385af9c6ba760
jason.hua <jason.hua@PC-JASONHUA>
2019-07-09 16:43:20 +0800

    递归和循环:跳台阶

---
+++ CodingInterviews/CodingInterviews/Coding008.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    /// <summary>
+    /// 递归和循环:跳台阶
+    /// 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+    /// </summary>
+    public class Coding008
+    {
+        /// <summary>
+        /// 循环跳台阶
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Jump(int n) {
+            if (n <= 2) {
+                return n;
+            }
+
+            int first = 1;
+            int second = 2;
+            int result = 0;
+            for (int i = 3; i <= n; i++) {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CodingInterviews/CodingTest/Coding008Test.cs
@@ -0,0 +1,42 @@
+﻿using CodingInterviews;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingTest
+{
+    public class Coding008Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, Coding008.Jump(1));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(2, Coding008.Jump(2));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal(3, Coding008.Jump(3));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.Equal(5, Coding008.Jump(4));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Assert.Equal(8, Coding008.Jump(5));
+        }
+
+    }
+}
